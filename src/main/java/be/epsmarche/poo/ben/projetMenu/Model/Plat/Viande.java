8baa8204f7337fc94b86777309efaa8f7778985f
Ben KAKE <you@example.com>
2022-12-31 09:01:43 +0100

    des-Hardcodage des variables et commentation de la classe

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Plat/Viande.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Plat/Viande.java
@@ -1,24 +1,55 @@
 package be.epsmarche.poo.ben.projetMenu.Model.Plat;
 
+/**
+ * Cette classe représente un plat à base de viande
+ */
 public class Viande implements Iplat {
+	
+	//protected Double prix = 10.5;
+	protected Double prix;
+	//protected String type = "Steak de 200g de crocodile";
+	protected String type;
+	
+	//public String preparation = getPeparation();
 
-	protected Double prix = 10.5;
-	protected String type = "Steak de 200g de crocodile";
-
+	/**
+	 * Constructeurs de nouvels objets Viande
+	 */
+	
 	public Viande() {
 		super();
 	}
 
+	/**
+	 * @param type de viande
+	 */
 	public Viande(String type) {
 		super();
 		this.type = getType();
 	}
 	
+	/**
+	 * @param type
+	 * @param prix de la viande
+	 */
+	public Viande(String type, Double prix) {
+		super();
+		this.type = type;
+		this.prix = prix;
+	}
 	
-
+	/*
+	 * getters et setters
+	 */
 	@Override
-	public String getPeparation() {
-		return getType() + " mariné au piment africain";
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+	@Override
+	public String getType() {
+		return this.type;
 	}
 
 	@Override
@@ -30,19 +61,17 @@ public class Viande implements Iplat {
 		return this.prix;
 	}
 
-	@Override
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@Override
-	public String getType() {
-		return this.type;
-	}
-
+	/**
+	 * @return au format lisible le prix et le type de viande
+	 */
 	@Override
 	public String toString() {
 		return "Viande [prix=" + getPrix() + ", type=" + getType() + "]";
+	}
+	
+	@Override
+	public String getPeparation() {
+		return getType() + " mariné au piment africain";
 	}
 
 }
