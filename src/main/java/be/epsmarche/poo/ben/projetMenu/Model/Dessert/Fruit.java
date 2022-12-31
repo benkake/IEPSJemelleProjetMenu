106e3d2b5c342ba9750dda78e4473e8d1f32cfd4
Ben KAKE <you@example.com>
2022-12-31 13:28:37 +0100

    création de affichage du menu et de son sous-total

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Dessert/Fruit.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Dessert/Fruit.java
@@ -9,48 +9,70 @@ import be.epsmarche.poo.ben.projetMenu.Patterns.*;
  * @author ben
  */
 public class Fruit extends PlatDecorator {
-	/*
-	 * Prix du dessert au fruit
-	 */
-	protected Double prixDessFruit;
-	/*
-	 * Type du dessert au fruit
-	 */
-	protected String typeDessFruit;
+	
 	/**
-	 * @param platDecore
-	 * @param prixDessFruit
-	 * @param typeFruit
+	 * @param prixDstFruit = prix du dessert au fruit
 	 */
-	public Fruit(Iplat platDecore, Double prix, String type) {
-		super(platDecore);
-		this.prixDessFruit = prix;
-		this.typeDessFruit = type;
+	protected Double prixDstFruit;
+	
+	/**
+	 * @param TypeDstFruit = type de dessert au fruit
+	 */
+	protected String typeDstFruit;
+	
+	/**
+	 * Constructeur de dessert
+	 * @param menu
+	 * @param prix
+	 * @param type
+	 */
+	public Fruit(Iplat menu, String type,Double prix ) {
+		super(menu);
+		this.prixDstFruit = prix;
+		this.typeDstFruit = type;
 	}
 	
+	/**
+	 * @return le plat garni avec le type de dessert
+	 */
 	@Override
 	public String getPeparation() {
 		return platDecore.getPeparation()+ "\nDessert: "+getType();
 	}
-
+	
+	/**
+	 * Getters et setters
+	 */
+	
 	@Override
 	public void setPrix(Double prix) {
-		this.prixDessFruit = prix;
+		this.prixDstFruit = prix;
 	}
 
+	/**
+	 * @return le prix le prix du plat + le prix de l'accompagnement + le prix du dessert
+	 */
 	@Override
 	public Double getPrix() {
-		return  platDecore.getPrix() + this.prixDessFruit;
+		return  platDecore.getPrix() + this.prixDstFruit;
 	}
 
 	@Override
 	public void setType(String type) {
-		this.typeDessFruit = type;	
+		this.typeDstFruit = type;	
 	}
 
 	@Override
 	public String getType() {
-		return this.typeDessFruit;
+		return this.typeDstFruit;
+	}
+	
+	/**
+	 * Affiche le menu comprenant le plat + accompagnement + dessert
+	 */
+	@Override
+	public String toString() {
+		return "<> " + getPeparation() +"\n";
 	}
 
 }
