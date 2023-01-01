a2f6ab37852815eb831254b865c3ad46d8ec3faf
Ben KAKE <you@example.com>
2023-01-01 21:24:12 +0100

    création paramètre tyPeFrites + ajout des paramètres typeFrites et prixFrites au constructeur + commentation

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Accompagnements/Frites.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Accompagnements/Frites.java
@@ -3,28 +3,58 @@ package be.epsmarche.poo.ben.projetMenu.Model.Accompagnements;
 import be.epsmarche.poo.ben.projetMenu.Model.Plat.*;
 import be.epsmarche.poo.ben.projetMenu.Patterns.*;
 
+/**
+ * Classe définissant un accompagnement à base de type Frites
+ * @author ben
+ */
 public class Frites extends PlatDecorator {
-
-	protected Double prixFrites = 4.5;
-
+	
+	/**
+	 * Prix de l'accompagnement à base  de frites
+	 */
+	protected Double prixFrites;
+	
+	/**
+	 * Type d'accompagnement à base de frites
+	 */
+	protected String typeFrites;
+	
+	/**
+	 * Constructeur à 1 paramètre
+	 * @param platDecore
+	 */
 	public Frites(Iplat platDecore) {
 		super(platDecore);
 		this.platDecore = platDecore;
 	}
-
+	
+	/**
+	 * Constructeur à 2 paramètres
+	 * @param menu
+	 * @param typeFrites
+	 * @param prixFrites
+	 */
+	public Frites(Iplat menu, String typeFrites,Double prixFrites ) {
+		super(menu);
+		this.typeFrites = typeFrites;
+		this.prixFrites = prixFrites;
+	};
+	
+	/**
+	 *@return un plat avec un accompagnement à base de Frites
+	 */
 	@Override
-	public String getPeparation() {
-		return platDecore.getPeparation() + " + frites de patate douce";
+	public String getPeparation() {           
+		return platDecore.getPeparation() +" \n >Accompagnement: "+ this.getType();
 	}
 
 	@Override
 	public void setPrix(Double prix) {
-
+		this.prixFrites = prix;
 	}
 
-	/*
-	 * getPrix returne le prix des frites (attribut local) plus le prix de la viande
-	 * (attribut hérité obtenu via la classe platDecore)
+	/**
+	 * @return le prix du plat + le prix de l'accompagnement à base de frites
 	 */
 	@Override
 	public Double getPrix() {
@@ -32,16 +62,19 @@ public class Frites extends PlatDecorator {
 	}
 
 	@Override
-	public void setType(String type) {
-		// TODO Auto-generated method stub
+	public void setType(String type) {  
+		this.typeFrites = type;
 	}
 
 	@Override
-	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getType() { 
+		return typeFrites;
 	}
-
+	
+	/**
+	 *@return sous la forme lisible, 
+	 *un plat avec un accompagnement à base de Frites + la somme des prix du plat et de cet accompagnement
+	 */
 	@Override
 	public String toString() {
 		return "- " + getPeparation() + " [Prix = " + getPrix() + " euros]\n";
