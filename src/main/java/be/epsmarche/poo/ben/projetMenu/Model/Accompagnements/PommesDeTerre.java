643239d949b54d947fa521a12f1b84e169142292
Ben KAKE <you@example.com>
2023-01-01 20:48:31 +0100

    création paramètre tyPommesDt + ajout des paramètres typePommesDT et prixPommeDt au constructeur + commentation

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Accompagnements/PommesDeTerre.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Accompagnements/PommesDeTerre.java
@@ -3,45 +3,78 @@ package be.epsmarche.poo.ben.projetMenu.Model.Accompagnements;
 import be.epsmarche.poo.ben.projetMenu.Model.Plat.*;
 import be.epsmarche.poo.ben.projetMenu.Patterns.*;
 
+/**
+ * Classe définissant un accompagnement à base de type pomme de terre
+ * @author ben
+ */
 public class PommesDeTerre extends PlatDecorator {
 
-	protected Double prixPommes = 3.5;
-
+	/**
+	 * Prix de l'accompagnement à base de pomme de terre
+	 */
+	protected Double prixPommesDT;
+	/**
+	 * Type d'accompagnement à base de pomme de terre
+	 */
+	protected String typePommesDT;
+	
+	/**
+	 * Constructeur à 1 paramètre
+	 * @param platDecore
+	 */
 	public PommesDeTerre(Iplat platDecore) {
 		super(platDecore);
 		this.platDecore = platDecore;
 	}
-
+	
+	public PommesDeTerre(Iplat platDecore, String typePommesDT,Double prixPommesDT) {
+		super(platDecore);
+		this.typePommesDT = typePommesDT;
+		this.prixPommesDT = prixPommesDT;
+		
+		
+	}
+	
+	/**
+	 *@return un plat avec un accompagnement à base de pomme de terre
+	 */
 	@Override
 	public String getPeparation() {
-		return platDecore.getPeparation() + " + pomme de terre cuite au four";
+		return platDecore.getPeparation() + " \n >Accompagnement: "+getType();
 	}
+	
+//	public String getPeparation() {
+//		return platDecore.getPeparation() + " + pomme de terre cuite au four";
+//	}
 
 	@Override
 	public void setPrix(Double prix) {
 
 	}
 
-	/*
-	 * getPrix returne le prix des Pomme de terre (attribut local) plus le prix de
-	 * la viande (attribut hérité obtenu via la classe platDecore)
+	/**
+	 * @return le prix du plat + le prix de l'accompagnement à base de pomme de terre
 	 */
 	@Override
 	public Double getPrix() {
-		return (platDecore.getPrix() + prixPommes);
+		return (platDecore.getPrix() + prixPommesDT);
 	}
 
 	@Override
 	public void setType(String type) {
-		// TODO Auto-generated method stub
+		this.typePommesDT= type;
 
 	}
 
 	@Override
 	public String getType() {
-		return null;
+		return this.typePommesDT;
 	}
-
+	
+	/**
+	 *@return sous la forme lisible, 
+	 *un plat avec un accompagnement à base de Pomme de terre + la somme des prix du plat et de cet accompagnement
+	 */
 	@Override
 	public String toString() {
 		return "- " + getPeparation() + " [Prix = " + getPrix() + " euros]\n";
