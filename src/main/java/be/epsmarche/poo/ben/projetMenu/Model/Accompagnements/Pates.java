c54d78e48a29814029c34576d9fd5e7b46a6a343
Ben KAKE <you@example.com>
2023-01-01 21:12:35 +0100

    création paramètre tyPePates + ajout des paramètres typePates et prixPates au constructeur + commentation

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Accompagnements/Pates.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Accompagnements/Pates.java
@@ -3,28 +3,61 @@ package be.epsmarche.poo.ben.projetMenu.Model.Accompagnements;
 import be.epsmarche.poo.ben.projetMenu.Model.Plat.*;
 import be.epsmarche.poo.ben.projetMenu.Patterns.*;
 
+/**
+ * Classe définissant un accompagnement à base de type Pates
+ * @author ben
+ */
 public class Pates extends PlatDecorator {
 
-	protected Double prixPates = 2.5;
+	/**
+	 * Prix de l'accompagnement à base de pates
+	 */
+	protected Double prixPates;
+	/**
+	 * Type d'accompagnement à base de pates
+	 */
+	protected String typePates;
 
+	/**
+	 * Constructeur à 1 paramètre
+	 * @param platDecore
+	 */
 	public Pates(Iplat platDecore) {
 		super(platDecore);
 		this.platDecore = platDecore;
 	}
-
+	
+	
+	/**
+	 * Constructeur à 2 paramètres
+	 * @param platDecore
+	 * @param prixPates
+	 * @param typePates
+	 */
+	public Pates(Iplat platDecore, Double prixPates, String typePates) {
+		super(platDecore);
+		this.prixPates = prixPates;
+		this.typePates = typePates;
+	}
+	
+	/**
+	 *@return un plat avec un accompagnement à base de pates
+	 */
 	@Override
 	public String getPeparation() {
-		return platDecore.getPeparation() + " + spaguetti à la Bolognaise";
+		return platDecore.getPeparation() + " \n >Accompagnement: "+getType();
 	}
+//	public String getPeparation() {
+//		return platDecore.getPeparation() + " + spaguetti à la Bolognaise";
+//	}
 
 	@Override
 	public void setPrix(Double prix) {
-
+		this.prixPates = prix;
 	}
 
-	/*
-	 * getPrix returne le prix des Pates (attribut local) plus le prix de la viande
-	 * (attribut hérité obtenu via la classe platDecore)
+	/**
+	 * @return le prix du plat + le prix de l'accompagnement à base de Pates
 	 */
 	@Override
 	public Double getPrix() {
@@ -33,16 +66,19 @@ public class Pates extends PlatDecorator {
 
 	@Override
 	public void setType(String type) {
-		// TODO Auto-generated method stub
+		this.typePates = type;
 
 	}
 
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.typePates;
 	}
-
+	
+	/**
+	 *@return sous la forme lisible, 
+	 *un plat avec un accompagnement à base de pates + la somme des prix du plat et de cet accompagnement
+	 */
 	@Override
 	public String toString() {
 		return "- " + getPeparation() + " [Prix = " + getPrix() + " euros]\n";
