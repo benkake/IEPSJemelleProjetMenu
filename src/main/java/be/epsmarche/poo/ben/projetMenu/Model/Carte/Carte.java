3360a5862fd656b29252218685b7c92e2a646087
Ben KAKE <you@example.com>
2023-01-06 20:20:12 +0100

    Création des methodes getPlatChoisi, getAccompagnementChoisi, getDessertChoisi

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Carte/Carte.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Carte/Carte.java
@@ -1,23 +1,24 @@
 package be.epsmarche.poo.ben.projetMenu.Model.Carte;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Classe définissant l'ensemble des cartes selectionnables dans le restaurant
  */
 public class Carte {
     /**
-     * @param Listes des choix de plats diponibles
+     * Listes des choix de plats diponibles
      */
     private ArrayList<Choix> listeDesPlats;
 
     /**
-     *@param Listes des choix d'accompagnements diponibles
+     *Listes des choix d'accompagnements diponibles
      */
     private ArrayList<Choix> listeDesAccompagnements;
 
     /**
-     *@param Listes des choix de desserts diponibles
+     * Listes des choix de desserts diponibles
      */
     private ArrayList<Choix> listeDesDesserts;
 
@@ -37,10 +38,55 @@ public class Carte {
 
     /**
      *sous-classe  Singleton
-     * qui permet d'avoir une carte unique dans un restaurant
+     * qui permet d'avoir un composant unique du menu (plat ou accompagnement ou dessert)
      */
     private static class CarteGetter{
         private static final Carte INSTANCE = new Carte();
+    }
+
+    /**
+     *
+     * @param idPlat
+     * @return un choix de plat
+     */
+    public Choix getPlatChoisi(String idPlat){
+        Choix platChoisi = null;
+        Iterator<Choix> it = listeDesPlats.iterator();
+        while(it.hasNext()){
+            Choix choix = it.next();
+            if(choix.getId().equals(idPlat)){
+                platChoisi = choix;
+            }
+        }
+        return platChoisi;
+    }
+
+    /**
+     * @param idAccomp
+     * @return un choix d'accompagnement
+     */
+    public Choix getAccompagnementChoisi(String idAccomp){
+        Choix accompagnementChoisi = null;
+        Iterator<Choix> it = listeDesAccompagnements.listIterator();
+        while(it.hasNext()){
+            Choix choix = it.next();
+            if(choix.getId().equals(idAccomp)){
+                accompagnementChoisi = choix;
+            }
+        }
+        return accompagnementChoisi;
+    }
+
+    public Choix getDessertChoisi(String idDessert){
+        Choix dessertChoisi =null;
+        Iterator<Choix> it = listeDesDesserts.iterator();
+        while(it.hasNext()){
+            Choix choix = it.next();
+            if(choix.getId().equals(idDessert)){
+                dessertChoisi = choix;
+            }
+        }
+        return dessertChoisi;
     }
 
 }
