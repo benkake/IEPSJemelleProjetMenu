22bee5a040d65acfe870edea069a77acdae9a32c
Ben KAKE <you@example.com>
2023-01-08 12:49:27 +0100

    creation methodes creationPlat(), creationAccompagnement(), creationDessert()

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Carte/Loader.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Carte/Loader.java
@@ -52,7 +52,7 @@ public class Loader {
 	}
 	
 	/**
-	 * Création d'une liste d'objet de type Choix (des catégories de plat)
+	 * Création d'une liste d'objet de type Choix des catégories de plat
 	 */
 	public void creationPlats() {
 		// Creation d'une liste de plats
@@ -69,7 +69,7 @@ public class Loader {
 		// récupérer les éléments catégories de plat
 		while(it1.hasNext()){
 			Element elementCategoriePlat = (Element) it1.next(); 
-			String attrCategorieValue = elementCategoriePlat.getAttributeValue("id");
+			String valeurAttrCategorie = elementCategoriePlat.getAttributeValue("id");
 			// récupération des noeuds enfants de catégorie
 			List listeCategoriePlat = elementCategoriePlat.getChildren();
 			Iterator it2 = listeCategoriePlat.iterator();
@@ -77,14 +77,93 @@ public class Loader {
 			// récupérer les elements plat
 			while(it2.hasNext()) {
 				Element elementPlat = (Element) it2.next();
-				String elementDeLaCategorie = elementPlat.getName();
+				String categorieDePlat = elementPlat.getName();
+				String id = elementPlat.getAttributeValue("id");
+				String description = elementPlat.getChildText("type");
+				Double prix = Double.parseDouble(elementPlat.getChildText("prix"));
+				Choix choixDePlat = new Choix(id, categorieDePlat, valeurAttrCategorie, prix, description);
+				listeDePlats.add(choixDePlat);
 			}
 		}
+		Carte carte = Carte.getInstance();
+		carte.setListeDesPlats(listeDePlats);
+	}
+	
+	/**
+	 * Création d'une liste d'objet de type Choix des catégories d'accompagnement
+	 */
+	public void creationAccompagnements() {
+		// Creation d'une liste d'accompagnements
+		ArrayList<Choix> listeAccompagnements = new ArrayList<>();
+		// Liste du noeud accompagnement 
+		//qui se situe au rang zero du tableau menu
+		Element elementAccompagnement = racine.getChildren("accompagnements").get(0);
+		// recuperation des noeuds enfants du noeud accompagnement
+		List listeTypesAccompagnement = elementAccompagnement.getChildren();
+		//Création d'un iterateur pour parcourir la liste des types d'accompagnement
+		Iterator it1 = listeTypesAccompagnement.iterator();
 		
+		// parcours du tableau listeTypesDacompanemet pour
+		// récupérer les éléments catégories d'accompagnement
+		while(it1.hasNext()){
+			Element elementCategorieAccomp = (Element) it1.next(); 
+			String valeurAttrCategorie = elementCategorieAccomp.getAttributeValue("id");
+			// récupération des noeuds enfants de catégorie
+			List listeCategorieAccomp = elementCategorieAccomp.getChildren();
+			Iterator it2 = listeCategorieAccomp.iterator();
+			// parcours du tableau listeCategorieAccomp pour
+			// récupérer les elements accompagnement
+			while(it2.hasNext()) {
+				Element elementAccomp = (Element) it2.next();
+				String categorieAccomp = elementAccomp.getName();
+				String id = elementAccomp.getAttributeValue("id");
+				String description = elementAccomp.getChildText("type");
+				Double prix = Double.parseDouble(elementAccomp.getChildText("prix"));
+				Choix choixDacompagnement = new Choix(id, categorieAccomp, valeurAttrCategorie, prix, description);
+				listeAccompagnements.add(choixDacompagnement);
+			}
+		}
+		Carte carte = Carte.getInstance();
+		carte.setListeDesAccompagnements(listeAccompagnements);
+	}
+	
+	/**
+	 * Création d'une liste d'objet de type Choix des catégories de dessert
+	 */
+	public void creationDessert() {
+		// Creation d'une liste de desserts
+		ArrayList<Choix> listeDeDesserts = new ArrayList<>();
+		// Liste du noeud Dessert 
+		//qui se situe au rang zero du tableau menu
+		Element elementDessert = racine.getChildren("desserts").get(0);
+		// recuperation des noeuds enfants du noeud dessert
+		List listeTypesDessert = elementDessert.getChildren();
+		//Création d'un iterateur pour parcourir la liste des types de dessert
+		Iterator it1 = listeTypesDessert.iterator();
 		
-		
+		// parcours du tableau listeTypesDessert pour
+		// récupérer les éléments catégories de dessert
+		while(it1.hasNext()){
+			Element elementCategorieDessert = (Element) it1.next(); 
+			String valeurAttrDessert = elementCategorieDessert.getAttributeValue("id");
+			// récupération des noeuds enfants de catégorie
+			List listeCategorieDessert = elementCategorieDessert.getChildren();
+			Iterator it2 = listeCategorieDessert.iterator();
+			// parcours du tableau listeCategorieDessert pour
+			// récupérer les elements accompagnement
+			while(it2.hasNext()) {
+				Element elementDess = (Element) it2.next();
+				String categorieDessert = elementDess .getName();
+				String id = elementDess .getAttributeValue("id");
+				String description = elementDess .getChildText("type");
+				Double prix = Double.parseDouble(elementDess .getChildText("prix"));
+				Choix choixDeDessert = new Choix(id, categorieDessert, valeurAttrDessert, prix, description);
+				listeDeDesserts.add(choixDeDessert);
+			}
+		}
+		Carte carte = Carte.getInstance();
+		carte.setListeDesAccompagnements(listeDeDesserts);
 	}
 
-	
 
 }
