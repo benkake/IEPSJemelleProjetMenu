4d26a807ed4c4c4d1d56f846591f58176b5c0e17
Ben KAKE <you@example.com>
2023-02-25 03:07:41 +0100

    methode getMenuConcocted OK

--- src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Commandes/Commande.java
+++ src/main/java/be/epsmarche/poo/ben/projetMenu/Model/Commandes/Commande.java
@@ -1,23 +1,27 @@
 package be.epsmarche.poo.ben.projetMenu.Model.Commandes;
 
 import java.util.ArrayList;
-
+import be.epsmarche.poo.ben.projetMenu.Model.Carte.Choix;
+import be.epsmarche.poo.ben.projetMenu.Model.Patterns.AccompagnementFactory;
+import be.epsmarche.poo.ben.projetMenu.Model.Patterns.DessertFactory;
+import be.epsmarche.poo.ben.projetMenu.Model.Patterns.PlatFactory;
 import be.epsmarche.poo.ben.projetMenu.Model.Plat.Iplat;
 
 /**
  * Classe définissant les commandes
+ * 
  * @author ben
  */
 public class Commande {
 	/**
-	 *numTab = le numéro de table
+	 * numTab = le numéro de table
 	 */
 	private String numTab;
-	
+
 	/**
-	 *listeMenu: la liste des menus
+	 * listeMenu: la liste des menus
 	 */
-	private ArrayList <Iplat> listeMenu; 
+	private ArrayList<Iplat> listeMenu = new ArrayList<>();
 
 	/**
 	 * Constructeur par défaut
@@ -28,6 +32,7 @@ public class Commande {
 
 	/**
 	 * Constructeur à 1 param
+	 * 
 	 * @param numTab
 	 */
 	public Commande(String numTab) {
@@ -48,40 +53,66 @@ public class Commande {
 		return listeMenu;
 	}
 
-	public void setListeCde(ArrayList<Iplat> listeMenu) {
+	public void setListeMenu(ArrayList<Iplat> listeMenu) {
 		this.listeMenu = listeMenu;
 	}
-	
+
 	/**
-	 * @param menu
-	 * ajoute les menus dans la liste des menus( la commande)
+	 * @param menu ajoute les menus dans la liste des menus( la commande)
 	 */
 	public void addMenu(Iplat menu) {
 		listeMenu.add(menu);
 	}
 	
+	public void removeLastMenu() {
+		int ind = getListeMenu().size()-1;
+		getListeMenu().remove(ind);
+	}
+
+	private Object lastIndexOf(ArrayList<Iplat> listeMenu2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	/**
 	 * @return l'ensemble de la commande sous forme de chaîne de caractères
 	 */
-	
-	public String displayCommand(){
-		Iplat tmp;
-		String text ="***********Commande de la table "+getNumTab()+" ********************\n\n";
-		for(int i = 0; i < getListeMenu().size(); ++i) { 
-			 tmp = (Iplat) (getListeMenu().get(i)); 
-			  text += tmp.toString()+"-----------------------\n";
+
+	public String displayCommand() {
+		Iplat tmp = null;
+		String text = null;
+		 text = "***********Commande de la table " + getNumTab() + " ********************\n\n";
+		for (int i = 0; i < getListeMenu().size(); ++i) {
+			tmp = (Iplat) (getListeMenu().get(i));
+			text += tmp.toString() + "-----------------------\n";
 		}
-		return text +"Prix total: €"+getPrixTotal();	
+		return text + "Prix total: €" + getPrixTotal();
+		
 	}
-	
+
 	/**
 	 * @return le prix total de toute la commande
 	 */
 	public Double getPrixTotal() {
-		Double prixTot =0.;
-		for(int i = 0; i < listeMenu.size();++i) {
+		Double prixTot = 0.;
+		for (int i = 0; i < listeMenu.size(); ++i) {
 			prixTot += listeMenu.get(i).getPrix();
 		}
 		return prixTot;
 	}
+
+	public Iplat getMenuConcocted(Choix plat, Choix accomp, Choix dess) {
+
+		AccompagnementFactory accompChoisi = new AccompagnementFactory();
+		DessertFactory dessChoisi = new DessertFactory();
+		PlatFactory platChoisi = new PlatFactory();
+		
+		Iplat menuConcocted = platChoisi.getPlat(plat.getType(), plat.getDescription(), plat.getPrix());
+		menuConcocted = accompChoisi.getAccomp(menuConcocted, accomp);
+		menuConcocted = dessChoisi.getDess(menuConcocted, dess);
+		
+		return menuConcocted;
+	}
+
+	
 }
